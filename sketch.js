be7325c47ab974bd16980bfcde5a98cb39b45a3c
Dhruv-Sunil <71018532+Dhruv-Sunil@users.noreply.github.com>
2020-10-01 14:14:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var packageBody,ground,log1,log2,log3
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -27,18 +27,33 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
+	log1sprite=createSprite(400,650, 200,20);
+	log1sprite.shapeColor=color("red")
+
+	log2sprite=createSprite(500,650, 20,100);
+	log2sprite.shapeColor=color("red")
+
+	log3sprite=createSprite(300,650, 20,100);
+	log3sprite.shapeColor=color("red")
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:false});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {friction:10000,restitution:0.2, isStatic:false});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
-
+	log1 = Bodies.rectangle(400, 650, 200, 20 , {isStatic:true} );
+	 World.add(world, log1);
+	 
+	 log2 = Bodies.rectangle(500, 650, 20, 100 , {isStatic:true} );
+	 World.add(world, log2);
+	 
+	 log3 = Bodies.rectangle(300, 650, 20, 100 , {isStatic:true} );
+ 	World.add(world, log3);
 
 	Engine.run(engine);
   
