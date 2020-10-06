5d75838e910f545046e6ddbcc076d8607a70b5ad
Dhruv-Sunil <71018532+Dhruv-Sunil@users.noreply.github.com>
2020-10-06 15:54:08 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,50 +1,49 @@
-
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-var ball,dustbin,ground;
 
-function preload()
-{
-	
+var engine, world;
+var ground,ball;
+var binImg,bin;
+
+function preload(){
+    binImg = loadImage("Images/dustbingreen.png");
+}
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground();
+    crumpledPaper = new Paper();
+
+    bin = createSprite(964,520,20,20);
+    bin.addImage(binImg);
+    bin.scale = 0.45;
+
+    binPart1 = new Dustbin(902,505,10,120);
+    binPart2 = new Dustbin(962,565,130,10);
+    binPart3 = new Dustbin(1024,505,10,120);
 }
 
-function setup() {
-	createCanvas(1200, 400);
-	engine = Engine.create();
-	world = engine.world;
-	ball=new Ball(75,300);
-	ground=new Ground(600,380,1200,20);
-	side1=new Dustbin(900,310,20,100);
-	side2=new Dustbin(1100,310,20,100);
-side3=new Dustbin(1000,350,200,20);
-	
+function draw(){
+    background(0);
+    Engine.update(engine);
 
-	//Create the Bodies Here.
+    //text(mouseX+","+mouseY,200,200);
 
-
-	Engine.run(engine);
-  
+    
+    ground.display();
+    crumpledPaper.display();
+    binPart1.display();
+    binPart2.display();
+    binPart3.display(); 
+      
+    drawSprites();
 }
 
-
-function draw() {
-  rectMode(CENTER);
-  background(0);
-  ball.display();
-  side1.display();
-  side2.display();
-  side3.display();
-  ground.display();
-  
-  drawSprites();
+function keyPressed(){
+    if(keyCode === UP_ARROW){
+        Matter.Body.applyForce(crumpledPaper.body,crumpledPaper.body.position,{x:74,y:-75});
+    }
 }
-function keyPressed(){ 
-	if(keyCode === UP_ARROW){
-		 Body.applyForce(ball.body,ball.body.position,{x:85,y:-85}); 
-		} 
-	}
-
-
-
